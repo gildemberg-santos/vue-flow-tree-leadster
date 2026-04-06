7ccc469772ebe071a62817117667d864f1775087
Gildemberg Santos <gildemeber.santos@gmail.com>
2026-04-06 12:54:37 -0300

    Refactor Vite config to support test and build modes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,41 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    include: ["src/**/*.test.js"],
-  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-});
+  ...(mode === 'test' ? {
+    test: {
+      environment: "jsdom",
+      globals: true,
+      include: ["src/**/*.test.js"],
+    },
+  } : {
+    build: {
+      lib: {
+        entry: fileURLToPath(new URL("./src/index.js", import.meta.url)),
+        name: "VueFlowTreeLeadster",
+        fileName: "vue-flow-tree-leadster",
+        formats: ["es", "umd"],
+      },
+      rollupOptions: {
+        external: ["vue", "@vue-flow/core", "@vue-flow/controls", "@vue-flow/minimap", "@vue-flow/background"],
+        output: {
+          globals: {
+            vue: "Vue",
+            '@vue-flow/core': 'VueFlow',
+            '@vue-flow/controls': 'VueFlowControls',
+            '@vue-flow/minimap': 'VueFlowMinimap',
+            '@vue-flow/background': 'VueFlowBackground',
+          },
+          exports: 'named',
+        },
+      },
+    },
+  }),
+}));
