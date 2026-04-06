991e4d6d4b1308493ad6e387381a03a642ed421b
Gildemberg Santos <gildemeber.santos@gmail.com>
2026-04-06 12:27:37 -0300

    Add FlowTree component and related utilities

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './components/FlowTree/style.css'
+
+export { default as FlowTree } from './components/FlowTree'
+
+export { useFlowTree } from './composables/useFlowTree'
+export { useFlowColors } from './composables/useFlowColors'
+export { useFlowTheme } from './composables/useFlowTheme'
+export { useFlowSettings } from './composables/useFlowSettings'
